b86445ce0849f5fa057ac6441585ca1b98fc9668
Felix Schaffernicht <felix@schaffernicht.com>
2019-07-01 20:31:21 +0200

    Add folder structure

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import About from './About'
+import Comissions from './Comissions'
+import Contact from './Contact'
+import CurrentWorks from './CurrentWorks'
+import Distribution from './Distribution'
+import Fanart from './Fanart'
+import IamMangaka from './IamMangaka'
+import Imprint from './Imprint'
+import LovedPeople from './LovedPeople'
+import Portfolio from './Portfolio'
+import Sale from './Sale'
+import SocialMedia from './SocialMedia'
+import Teacher from './Teacher'
+import Workshops from './Workshops'
+
+export {
+  About,
+  Comissions,
+  Contact,
+  CurrentWorks,
+  Distribution,
+  Fanart,
+  IamMangaka,
+  Imprint,
+  LovedPeople,
+  Portfolio,
+  Sale,
+  SocialMedia,
+  Teacher,
+  Workshops
+}
